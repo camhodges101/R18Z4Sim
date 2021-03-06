0585a617c1b16d457d1286999112344444d77e7d
camhodges@gmail.com <camhodges@gmail.com>
2021-03-06 07:17:50 +0000

    rpbuild

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/cameron/Documents/cppTutorials/R18Z4Sim
+# Install script for directory: /home/pi/cppTutorials/R18Z4Sim
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/cameron/Documents/cppTutorials/R18Z4Sim/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/pi/cppTutorials/R18Z4Sim/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
