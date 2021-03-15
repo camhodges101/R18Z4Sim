20b156980b4491161d0f457b757d28f9359633fe
camhodges101 <camhodges@gmail.com>
2021-03-15 08:36:59 +0800

    fixed gui

--- gui/interface.py
+++ gui/interface.py
@@ -39,34 +39,34 @@ class interface():
         self.parameter3voltage.grid(row=4,column=3)
 
         self.parameter4Label=tk.Label(self.parameters,text="MAF   ")
-        self.parameter4Label.grid(row=4,column=0)
+        self.parameter4Label.grid(row=5,column=0)
         self.parameter4Data = tk.Entry(self.parameters,width =10)
-        self.parameter4Data.grid(row=4,column=2)
+        self.parameter4Data.grid(row=5,column=2)
         self.parameter4voltage = tk.Entry(self.parameters,width =10)
-        self.parameter4voltage.grid(row=4,column=3)
+        self.parameter4voltage.grid(row=5,column=3)
 
         self.parameter5Label=tk.Label(self.parameters,text="Intake Air Temp   ")
-        self.parameter5Label.grid(row=5,column=0)
+        self.parameter5Label.grid(row=6,column=0)
         self.parameter5Data = tk.Entry(self.parameters,width =10)
-        self.parameter5Data.grid(row=5,column=2)
+        self.parameter5Data.grid(row=6,column=2)
         self.parameter5voltage = tk.Entry(self.parameters,width =10)
-        self.parameter5voltage.grid(row=5,column=3)
+        self.parameter5voltage.grid(row=6,column=3)
 
 
         self.parameter6Label=tk.Label(self.parameters,text="A/F Ratio   ")
-        self.parameter6Label.grid(row=6,column=0)
+        self.parameter6Label.grid(row=7,column=0)
         self.parameter6Data = tk.Entry(self.parameters,width =10)
-        self.parameter6Data.grid(row=6,column=2)
+        self.parameter6Data.grid(row=7,column=2)
         self.parameter6voltage = tk.Entry(self.parameters,width =10)
-        self.parameter6voltage.grid(row=6,column=3)
+        self.parameter6voltage.grid(row=7,column=3)
 
 
         self.parameter7Label=tk.Label(self.parameters,text="Water Temp   ")
-        self.parameter7Label.grid(row=7,column=0)
+        self.parameter7Label.grid(row=8,column=0)
         self.parameter7Data = tk.Entry(self.parameters,width =10)
-        self.parameter7Data.grid(row=7,column=2)
+        self.parameter7Data.grid(row=8,column=2)
         self.parameter7voltage = tk.Entry(self.parameters,width =10)
-        self.parameter7voltage.grid(row=7,column=3)
+        self.parameter7voltage.grid(row=8,column=3)
 
 
         self.controls=tk.LabelFrame(self.form,text="")
